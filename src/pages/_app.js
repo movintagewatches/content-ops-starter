fa16b00257d5acc5d50169dc1f04cf4a47686c7b
movintagewatches <movintagewatches@gmail.com>
2025-09-09 13:56:47 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
 import '../css/main.css';
+import { Playfair_Display, Sansation } from 'next/font/google';
+
+// Define fonts and expose CSS variables
+const heading = Playfair_Display({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-heading',
+  display: 'swap',
+});
+
+const body = Sansation({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: '--font-body',
+  display: 'swap',
+});
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  // Attach the variables at the root so Tailwind can use them
+  return (
+    <div className={`${body.variable} ${heading.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
